a9cc4e9f0665b8a9bd2493086bbeb783d2603ed8
ahmadcoderx <heyahmadhassan@gmail.com>
2025-07-15 19:58:38 +0500

    Integrate static file serving for Vite React build and enhance routing. Added path module for serving static files and updated the main route to serve the React app for non-API requests. Improved code formatting for consistency.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { setupSwagger } from "./swagger";
 import cors from "cors";
 import globalErrorHandler from "./utils/globalErrorHandler";
 import AppError from "./utils/appError";
+import path from "path";
 
 dotenv.config();
 const app = express();
@@ -14,28 +15,33 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(express.json());
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 setupSwagger(app);
-console.log("hi")
+console.log("hi");
 
-app.get("/", async (req, res) => {
-    res.json({
-        message: "Construction BE                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               API is working Now!",
-
-    });
-})
-
+// Serve static files from Vite React build (dist)
+const distPath = path.join(__dirname, "../fe-dist");
+app.use(express.static(distPath));
 
 app.use("/api", routes);
 
+// Serve React app for all non-API routes
+app.get(/^\/(?!api).*/, (req, res, next) => {
+  // If the request accepts HTML, serve index.html
+  if (req.accepts("html")) {
+    res.sendFile(path.join(distPath, "index.html"));
+  } else {
+    next();
+  }
+});
+
 app.all("*", (req, _res, next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 app.use(globalErrorHandler);
 
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
